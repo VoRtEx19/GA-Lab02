3be0ad0552466aceb03835aeb6af11488d18dbb0
VoRtEx19 <lasvetlichnyy@edu.hse.ru>
2023-10-07 14:16:39 +0300

    v 1.0
    - added view binding in gradle
    - changed gradle dependencies to newer versions
    - renamed main activity to Enter Activity
    - added elements in activity_enter.xml
    - added code in main activity

---
+++ app/src/main/java/com/example/lab_02/EnterActivity.java
@@ -0,0 +1,62 @@
+package com.example.lab_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.lab_02.databinding.ActivityEnterBinding;
+
+public class EnterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        com.example.lab_02.databinding.ActivityEnterBinding binding = ActivityEnterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        final EditText enter = binding.enterEdit;
+        final Button button = binding.submitButton;
+        final TextView show = binding.resultTextview;
+
+        TextWatcher afterTextChangedListener = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // ignore
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // ignore
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                show.setText(enter.getText().toString());
+            }
+        };
+        enter.addTextChangedListener(afterTextChangedListener);
+
+        button.setOnClickListener(v -> {
+            if (button.getText().toString().equals("Submit")) {
+                button.setText(R.string.change_string);
+                enter.setVisibility(View.INVISIBLE);
+                enter.setClickable(false);
+                show.setVisibility(View.VISIBLE);
+                show.setClickable(true);
+            } else {
+                button.setText(R.string.submit_string);
+                enter.setVisibility(View.VISIBLE);
+                enter.setClickable(true);
+                show.setVisibility(View.INVISIBLE);
+                show.setClickable(false);
+            }
+        });
+    }
+}
